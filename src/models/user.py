4e36f0439e621cdb06106d25b911a1130a0e581c
Brinza2011 <abrorakbarov0550@gmail.com>
2026-05-15 21:54:17 +0500

    upgraded register

--- src/models/user.py
+++ src/models/user.py
@@ -4,13 +4,17 @@ from sqlalchemy import Column, Integer, String
 from src.database.db import Base
 
 
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer
+
 class UserModel(Base):
     __tablename__ = "users"
 
-    id = Column(Integer,primary_key=True,index=True)
-    username = Column(String,nullable=False)
-    password = Column(String,nullable=False)
-    email = Column(String,nullable=False, unique=True)
+    id: Mapped[int] = mapped_column(Integer,primary_key=True,autoincrement=True)
+    role: Mapped[str] = mapped_column(default="USER")
+    username: Mapped[str]
+    password: Mapped[str]
+    email: Mapped[str]
 
     def __str__(self) -> str:
         return f"{self.username}"
