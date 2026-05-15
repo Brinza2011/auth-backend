fd98d8dd661cc43528af91c87248064b4d7d62af
Brinza2011 <abrorakbarov0550@gmail.com>
2026-05-15 20:01:30 +0500

    added function to login

---
+++ src/dependencies/token.py
@@ -0,0 +1,10 @@
+
+from src.service.token import TokenService
+from src.dependencies.jwt import get_jwt_service
+
+
+async def get_token_service() -> TokenService:
+    jwt_svc = await get_jwt_service()
+    token_svc = TokenService(jwt_svc)
+
+    return token_svc
--- src/service/jwt.py
+++ src/service/jwt.py
@@ -1,12 +1,14 @@
 
 
-from typing import TypedDict
+from datetime import UTC, datetime, timedelta
+from typing import Optional, TypedDict
 import jwt
 
 
 class JWTPayload(TypedDict):
-    user_id: int
+    sub: str
     role: str
+    exp: Optional[int]
 
 
 
@@ -18,9 +20,16 @@ class JWTService:
 
 
     def encode_token(self, payload: JWTPayload) -> str:
+        if not payload["exp"]:
+            payload["exp"] = 5
+
+
+        exp = datetime.now(UTC) + timedelta(minutes=payload["exp"])
+
         data = {
-            "user_id": payload["user_id"],
-            "role": payload["role"]
+            "sub": payload["sub"],
+            "role": payload["role"],
+            "exp": exp
         }
 
         return jwt.encode(data, self.secret_key, algorithm = "HS256")
@@ -30,8 +39,9 @@ class JWTService:
         try:
             decoded_payload = jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
             return {
-                "user_id": decoded_payload["user_id"],
-                "role": decoded_payload["role"]
+                "sub": decoded_payload["sub"],
+                "role": decoded_payload["role"],
+                "exp": decoded_payload["exp"]
             }
         
         except jwt.ExpiredSignatureError:
